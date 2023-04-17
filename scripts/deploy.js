0d8d319295d3ba26a1086d2efa21348449071637
bottledlactose <175543105+bottledlactose@users.noreply.github.com>
2023-04-17 20:07:35 +0200

    Organize project structure

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,11 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, guildId, token } = require('../config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
 
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(path.dirname(__dirname), 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {

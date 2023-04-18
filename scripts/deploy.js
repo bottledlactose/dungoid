c3a388a2ae0403756a044835ec1918f539df0753
bottledlactose <bottledlactose@gmail.com>
2023-04-18 20:16:44 +0200

    Push application commands to all servers

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -25,7 +25,7 @@ const rest = new REST().setToken(token);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+      Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 

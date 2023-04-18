78419c3a73978f0b9de5e70903b18682ed990214
bottledlactose <175543105+bottledlactose@users.noreply.github.com>
2023-04-18 23:18:16 +0200

    Refactor more stuff and add delete command

--- commands/create.js
+++ commands/create.js
@@ -8,18 +8,18 @@ const db = new QuickDB();
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('create')
-		.setDescription('Create a new alias or character')
+		.setDescription('Create a new character to speak as')
     .addStringOption(option =>
       option
         .setName('name')
-        .setDescription('What is the display name of your alias or character?')
+        .setDescription('What is the display name of your character?')
         .setRequired(true)
         .setMinLength(2)
         .setMaxLength(32))
     .addStringOption(option =>
       option
         .setName('prefix')
-        .setDescription('What is the chat prefix for your character?')
+        .setDescription('What is the identifier prefix for your character?')
         .setRequired(true)
         .setMaxLength(6))
     .addAttachmentOption(option =>
@@ -34,9 +34,30 @@ module.exports = {
 
     let characters = await characterModule.get(interaction.user);
 
+    if (characters.length + 1 >= 20) {
+
+      const embed = embedModule.error(client)
+        .setTitle('Whoops!')
+        .setDescription(`You may have up to 20 characters. You may delete characters using \`/delete\``);
+
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
+    if (!avatar.contentType.startsWith('image/')) {
+
+      const embed = embedModule.error(client)
+        .setTitle('Whoops!')
+        .setDescription(`The uploaded attachment must be an image!`);
+
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+
     for (const c of characters) {
       if (c.prefix === prefix) {
         const embed = embedModule.error(client)
+          .setTitle('Whoops!')
           .setDescription(`The prefix \`${prefix}\` is already in use by another character!`);
 
         await interaction.reply({ embeds: [embed], ephemeral: true });
@@ -51,7 +72,8 @@ module.exports = {
     }]);
 
     const embed = embedModule.success(client)
-      .setDescription(`You can now use your character by using \`/say ${prefix} Hello, world!\``);
+      .setTitle('Done!')
+      .setDescription('You can now use your character by using \`/say\`');
 
     await interaction.reply({ embeds: [embed], ephemeral: true });
 	},

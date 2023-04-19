68a30214bdc1dec7d0e640a02f5cc86c33214584
bottledlactose <175543105+bottledlactose@users.noreply.github.com>
2023-04-19 20:05:38 +0200

    Refactor and add missing comments

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
-const embedModule = require('../modules/embed');
-
 const { clientId } = require('../config.json');
+const embedModule = require('../modules/embed');
 
 module.exports = {
 	data: new SlashCommandBuilder()

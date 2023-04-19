8fff25cbedc4dc9aaf9c4582f745ceb39337c3c4
bottledlactose <175543105+bottledlactose@users.noreply.github.com>
2023-04-19 21:56:46 +0200

    Fix max character placeholder

--- commands/create.js
+++ commands/create.js
@@ -36,7 +36,7 @@ module.exports = {
 
       const embed = embedModule.error(client)
         .setTitle('Maximum characters reached!')
-        .setDescription('You may have up to 20 characters. You can delete some characters using **/delete**.');
+        .setDescription(`You may have up to ${config.maxCharacters} characters. You can delete some characters using **/delete**.`);
 
       await interaction.reply({ embeds: [embed], ephemeral: true });
       return;

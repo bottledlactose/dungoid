c809f03b5d5ec17e1f2af7088d6bae4d134e5704
bottledlactose <bottledlactose@gmail.com>
2023-04-20 22:56:32 +0200

    Add prefix rename migration

---
+++ migrations/20230420_rename-prefix.js
@@ -0,0 +1,22 @@
+const { QuickDB } = require('quick.db');
+const db = new QuickDB();
+
+const migration = async () => {
+
+  const charactersData = await db.startsWith('characters_');
+
+  for (const data of charactersData) {
+    for (const i in data.value) {
+
+      if ('prefix' in data.value[i]) {
+        data.value[i].tag = data.value[i].prefix;
+        delete data.value[i].prefix;
+      }
+    }
+
+    await db.set(data.id, data.value);
+    console.log(`${data.id} successfully migrated!`);
+  }
+}
+
+migration();

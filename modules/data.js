48bd85884e48de6150393eb6927ea7ffdfb9b235
bottledlactose <bottledlactose@gmail.com>
2023-04-22 14:23:48 +0200

    Refactor single character fetching and errors

--- modules/data.js
+++ modules/data.js
@@ -4,6 +4,17 @@ const db = new QuickDB();
 const charactersData = {
   get: async user => await db.get(`characters_${user.id}`) || [],
   set: async (user, characters) => await db.set(`characters_${user.id}`, characters),
+  single: async (user, tag) => {
+    const characters = await charactersData.get(user);
+
+    for (const i in characters) {
+      if (characters[i].tag === tag) {
+        return { character: characters[i], index: i }
+      }
+    }
+
+    return null;
+  }
 };
 
 const logData = {

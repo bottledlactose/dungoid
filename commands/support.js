dd6e5760f90be6505373bf89620344dca62eda00
bottledlactose <bottledlactose@gmail.com>
2023-05-08 19:43:56 +0200

    Update /support command

--- commands/support.js
+++ commands/support.js
@@ -1,17 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { infoEmbed } = require('../modules/embed');
-const { supportURL } = require('../config.json');
+const { clientId } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('support')
-		.setDescription('Receive a permanent invite link to my support channel'),
+		.setDescription('Receive a link to my listing page to get support or leave a rating'),
 	async execute(interaction, client) {
 
     const embed = infoEmbed(client)
-      .setTitle(`Need help?`)
-      .setDescription(`Need some help or do you want to report a bug or make a suggestion? Use [this link](https://discord.gg/${supportURL}) to join the ${client.user.username} support server!`);
+      .setTitle(`Need help or want to leave a rating?`)
+      .setDescription(`Do you need some help or do you want to report a bug or make a suggestion? Or would you be so kind to leave a rating on the bot? Please have a look at [${client.user.username}'s listing entry](https://top.gg/bot/${clientId}).`);
 
-		await interaction.reply({ embeds: [embed] });
+		await interaction.reply({ embeds: [embed], ephemeral: true });
 	},
 };
